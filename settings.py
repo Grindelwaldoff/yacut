e66128e9e7ad89572b541e687e7c51ea9ea8219c
Grindelwaldoff <vsevolodrybnik@gmail.com>
2023-09-28 20:07:58 +0300

    review

--- settings.py
+++ settings.py
@@ -2,6 +2,9 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE')
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('MODIFICATION')
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE', default='sqlite:///db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('MODIFICATION', default='False')
+    SECRET_KEY = os.getenv(
+        'SECRET_KEY',
+        default='qlwrkjsdlfjdlkxmvnawkjrqroisjdfkl23j4lkmsdf2kj'
+    )
